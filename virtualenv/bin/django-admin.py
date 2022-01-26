65a479013e37c128e625eb07b01c17618c0fee44
samuelassis <samuelassisvieira@gmail.com>
2022-01-26 11:20:14 -0300

    Iniciando Testes Funcionais

---
+++ virtualenv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/samuel/UFMG/7/Testes De Software/tdd-project/virtualenv/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
